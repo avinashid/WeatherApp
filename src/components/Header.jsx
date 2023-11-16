6587852b52a0547a4625f4990804fc5585b883fa
Avinash Kumar <avinashkumar532004@gmail.com>
2023-11-16 20:08:17 +0530

    added header and using tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-slate-400 flex justify-between px-6 py-2 text-white items-center ">
+      <div className="text-2xl font-bold  ">Weather 99</div>
+      <div>Refresh</div>
+    </div>
+  );
+};
+
+export default Header;
